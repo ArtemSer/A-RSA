8be5f6053798a1f4ba89b5fe49872b7ac481fb84
ArtemSer <artem.sierikov@gmail.com>
2018-05-20 20:03:30 +0300

    Refactoring ...

---
+++ functional.h
@@ -0,0 +1,132 @@
+#include <string>
+
+#ifndef A_RSA_FUNCTIONAL_H
+#define A_RSA_FUNCTIONAL_H
+
+#endif //A_RSA_FUNCTIONAL_H
+
+int displayMenu() {
+    int choice;
+    std::cout << "--- A-RSA ---\n"
+                 "1. Generate keys\n"
+                 "2. Encrypt message\n"
+                 "3. Decrypt message\n"
+                 "4. Exit\n"
+                 "option > ";
+    std::cin >> choice;
+    return choice;
+}
+
+void generateKeyPair() {
+    std::cout << "Generating keys ..." << std::endl << std::endl;
+    // Генерация двух чисел и выбор двух простых чисел.
+    srand((unsigned) time(nullptr));
+    // use c++ random library instead of Rand()
+    int p = rand() % 100,
+            q = rand() % 100,
+            p_simple = sundaram(p),
+            q_simple = sundaram(q);
+    //Находим число RsaModule.
+    auto RsaModule = static_cast<unsigned int>(p_simple * q_simple);
+    //Генерация числа exponentOfDecryption и проверка его на взаимопростоту
+    //с числом ((p_simple-1)*(q_simple-1)).
+    int exponentOfDecryption = 0, d_simple = 0;
+    while (d_simple != 1) {
+        exponentOfDecryption = rand() % 100;
+        d_simple = gcd(exponentOfDecryption, ((p_simple - 1) * (q_simple - 1)));
+    }
+    //Определение числа exponentOfEncryption, для которого является истинным
+    //соотношение (exponentOfEncryption*exponentOfDecryption)%((p_simple-1)*(q_simple-1))=1.
+    unsigned int exponentOfEncryption = 0, e_simple = 0;
+    while (e_simple != 1) {
+        exponentOfEncryption += 1;
+        e_simple = (exponentOfEncryption * exponentOfDecryption) % ((p_simple - 1) * (q_simple - 1));
+    }
+///Сгенерированные ключи.
+    std::cout << " - Open key: \t" << '[' << std::setw(6) << exponentOfEncryption << ',' << std::setw(6) << RsaModule
+              << ']' << std::endl;
+    std::cout << " - Secret key: \t" << '[' << std::setw(6) << exponentOfDecryption << ',' << std::setw(6) << RsaModule
+              << ']' << std::endl << std::endl;
+}
+
+void encryptMessage() {
+    int size, exponentOfEncryption, RsaModule;
+    std::cout << "Enter please the size of message: ";
+    std::cin >> size;
+    std::cout << "Enter please the Open key: ";
+    std::cin >> exponentOfEncryption;
+    std::cout << "Enter please the RsaModule ";
+    std::cin >> RsaModule;
+    std::cout << "Please enter the text to encrypt below." << std::endl;
+    std::cin.ignore();
+    auto *Text = new char[size];
+    std::cin.get(Text, size);
+    std::cout << "Text is = " << Text << std::endl;
+    std::cin.ignore();
+    auto *CryptoText = new unsigned int[size];
+    //Получение из введённых данных десятичного кода ASCII и
+    //дальнейшее его преобразование по формуле ci = (mj^exponentOfEncryption)%RsaModule.
+    int AsciiKey = 301;
+    int c;
+    std::cout << std::endl
+              << std::setw(5) << "Text" << std::setw(10) << "ASCII"
+              << std::setw(20) << "CryptoText/Block#" << "---------------------"
+              << std::endl;
+
+    for (int j = 0; j < size; j++) {
+        c = 1;
+        int ASCIIcode = (static_cast<int>(Text[j])) + AsciiKey;
+        for (unsigned int i = 0; i < exponentOfEncryption; i++) {
+            c *= ASCIIcode;
+            c %= RsaModule;
+        }
+        CryptoText[j] = static_cast<unsigned int>(c);
+        AsciiKey += 1;
+    }
+    for (int j = 0; j < size - 1; j++)
+        std::cout << std::setw(5) << Text[j]
+                  << std::setw(6) << static_cast<int>(Text[j])
+                  << std::setw(20) << CryptoText[j] << std::endl;
+    std::cout << "Your encrypted text is [ ";
+    for (int i = 0; i < size - 1; i++) std::cout<< CryptoText[i];
+    std::cout << size << " ]" << std::endl;
+    delete[] Text;
+    delete[] CryptoText;
+}
+void decryptMessage() {
+    int size, exponentOfDecryption, RsaModule;
+    std::cout << "Enter please the size of message: ";
+    std::cin >> size;
+    std::cout << "Enter please the Open key: ";
+    std::cin >> exponentOfDecryption;
+    std::cout << "Enter please the RsaModule ";
+    std::cin >> RsaModule;
+    std::cout << "Please enter the text to encrypt below." << std::endl;
+    std::cin.ignore();
+    auto *Text = new char[size];
+    std::cin.get(Text, size);
+    std::cout << "Text is = " << Text << std::endl;
+    std::cin.ignore();
+    auto *Tdecrypt = new unsigned int[size];
+    //Расшифровка полученного кода по формуле mi = (ci^exponentOfDecryption)%RsaModule
+    //и перевод его в десятичный код ASCII.
+
+    int AsciiKey = 301;
+    int m;
+    for (int j = 0; j < size; j++) {
+        m = 1;
+        for (unsigned int i = 0; i < exponentOfDecryption; i++) {
+            m = m * CryptoText[j];
+            m = m % RsaModule;
+        }
+        m = m - AsciiKey;
+        Tdecrypt[j] = static_cast<unsigned int>(m);
+        AsciiKey += 1;
+    }
+    for (int j = 0; j < size - 1; j++) {
+        cout << setw(5) << Text[j] << setw(6) << static_cast<int>(Text[j]) << setw(20)
+             << CryptoText[j] << setw(14) << Tdecrypt[j] << setw(14) << static_cast<char>(Tdecrypt[j]) << endl;
+    }
+    delete[] Tdecrypt;
+
+}
